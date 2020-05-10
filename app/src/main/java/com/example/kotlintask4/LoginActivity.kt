c9419b6ab72f7bb90ca3090249f257a5d718dd5a
Azeeza2 <balogunzzt@gmail.com>
2020-05-10 10:36:44 +0100

    Register page designed

--- app/src/main/java/com/example/kotlintask4/LoginActivity.kt
+++ app/src/main/java/com/example/kotlintask4/LoginActivity.kt
@@ -3,58 +3,90 @@ package com.example.kotlintask4
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import kotlinx.android.synthetic.main.login_activity.view.*
+import kotlinx.android.synthetic.main.login_page.*
 
 
-class LoginActivity : AppCompatActivity(){
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+    private val radioSexGroup: RadioGroup? = findViewById(R.id.radioGrp)
+    private var radioSexButton: RadioButton? = findViewById(R.id.radioM)
+    private val btnDisplay: Button? = null
+
 
     protected var loginButton: Button? = findViewById(R.id.button)
-    var b2:Button? = null
-   private val username: EditText? = findViewById(R.id.username_edittext)
-  private  val  password:EditText? = findViewById(R.id.password_edittext)
+    var b2: Button? = null
+    private val username: EditText? = findViewById(R.id.username_edittext)
+    private val password: EditText? = findViewById(R.id.password_edittext)
 
     var tx1: TextView? = null
     var counter = 3
 
+    /*  b2 = (Button)findViewById(R.id.button2);
+           tx1 = (TextView)findViewById(R.id.textView3);
+           tx1.setVisibility(View.GONE);*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
 
-      /*  b2 = (Button)findViewById(R.id.button2);
-        tx1 = (TextView)findViewById(R.id.textView3);
-        tx1.setVisibility(View.GONE);*/
+        loginButton?.setOnClickListener(this)
+        addListenerOnButton()
+        registerButton?.setOnClickListener(this)
 
-        loginButton?.setOnClickListener( View.OnClickListener() {
-            override fun onClick(view: View) {
-                if (!(!username?.getText().toString().equals("admin")) || !(!password?.getText().toString().equals("admin"))
-                ) {
-                    //correcct password
-                    Toast.makeText(applicationContext, "Redirecting......", Toast.LENGTH_SHORT)
-                        .show()
-                } else {
-                    //wrong password
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT)
-                        .show()
-                    username?.setVisibility(View.VISIBLE)
-                    username?.setBackgroundColor(Color.RED)
-                    counter--
-                    username?.setText(counter.toString())
+    }
 
-                    if (counter == 0) {
-                        loginButton?.setEnabled(false)
-                    }
-                }
+    override fun onClick(view: View) {
+
+        if (!(!username?.getText().toString().equals("admin")) || !(!password?.getText().toString()
+                .equals("admin"))
+        ) {
+            //correcct password
+            Toast.makeText(applicationContext, "Redirecting......", Toast.LENGTH_SHORT).show()
+        } else {
+            //wrong password
+            Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show()
+            username?.setVisibility(View.VISIBLE)
+            username?.setBackgroundColor(Color.RED)
+            counter--
+            username?.setText(counter.toString())
+
+            if (counter == 0) {
+                loginButton?.setEnabled(false)
             }
+        }
+    }
+
+    fun openSignupPage(view: View) {
+        var signup: TextView? = findViewById(R.id.sign_up)
+        signup?.setOnClickListener(View.OnClickListener() {
+            intent = findViewById(R.id.login_page)
+            startActivity(intent)
         })
+
+    }
+
+    fun addListenerOnButton() {
+
+        radioSexGroup?.setOnClickListener(View.OnClickListener() {
+            @Override
+            fun onClick(view: View) {
+                // get selected radio button from radioGroup
+                var selectedId = radioSexGroup.checkedRadioButtonId
+                // find the radiobutton by returned id
+                radioSexButton = findViewById(selectedId)
+
+                /*Toast.makeText(this,radioSexButton, Toast.LENGTH_SHORT).show()*/
+            }
+        });
+    }
+
+    fun register(view: View) {
+        finish()
     }
 }
+
+
 
        /* b2.setOnClickListener(new View.OnClickListener() {
             @Override
